a525000ad2f6198789b6c5acba3137684d72dfd9
Abbos <yulchiyevabbos1@gmail.com>
2021-03-17 22:50:59 +0500

    new Chnges

--- src/main/java/uz/pdp/appstudycenters/controller/SubCategoryController.java
+++
@@ -1,9 +0,0 @@
-package uz.pdp.appstudycenters.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(value = "/subCategory")
-public class SubCategoryController {
-}
